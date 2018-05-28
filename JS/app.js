c1b82382fd18346fa152a510b5213a5e7d9fe1b9
2Diomedes <t.martinteyssier@gmail.com>
2018-05-28 15:29:33 +0200

    Now calculator display numbers, but doesn't calculate.

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,30 @@
 var p = document.getElementsByTagName('p')[0];
-
+var vf = false;
+var equal = document.getElementById('equal');
+// var plus = "+";
+// var minus = "-";
+// var multiply = "*";
+// var divide =  "/";
 
 var btn = document.getElementsByTagName('button');
 
 for (var i = 0; i < btn.length; i++) {
   btnI = btn[i];
-  console.log(btnI.innerText);
-  btnI.addEventListener('click', function(){
-    p.innerText += this.innerText;
+  btnI.addEventListener('click', function() {
+    if (this.className == "number") {
+      p.innerText += this.innerText;
+    } else if (this.className == "operator" && vf == false) {
+      p.innerText += this.innerText;
+      vf = true;
+    } else if (this.className == "operator" && vf == true) {
+      alert("You can only use one operator")
+    }
   });
 }
+equal.addEventListener('click', function() {
+  if (p.innerText.indexOf("+")) {
+    console.log(p.innerText.indexOf("+"));
+
+  }
+  console.log("gagné");
+})
